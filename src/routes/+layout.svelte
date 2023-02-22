2b13053d78f89e8bcc939d88d8f884b7ea0c56ff
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-02-22 16:42:46 +0400

    Create navigation routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,14 @@
     import "../app.css";
 </script>
 
-<slot />
+<nav>
+  <a href="/">Home</a>
+  <a href="/mint">Mint/Redeem</a>
+  <a href="/members">Members</a>
+  <a href="/roles">SFT Roles</a>
+  <a href="/audits">Audits</a>
+  <a href="/asset-register">Asset Register</a>
+  <a href="/manual">Manual</a>
+</nav>
+
+<slot></slot>
