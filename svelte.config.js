083e2c11a2a7c74e0e948d89b7deb88ba5598ec3
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-02-28 19:06:24 +0400

    Use adapter auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-// import adapter from '@sveltejs/adapter-auto';
-import node from "@sveltejs/adapter-node";
+import adapter from '@sveltejs/adapter-auto';
+// import node from "@sveltejs/adapter-node";
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: node()
+		adapter: adapter()
 	},
 	preprocess: vitePreprocess()
 };
